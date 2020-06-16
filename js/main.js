2979fd48711df5c98f7f73240778ce6c499d0ce5
tegar97 <eategar@gmail.com>
2020-06-16 17:42:03 +0700

    reupload to github

---
+++ js/main.js
@@ -0,0 +1,26 @@
+
+var swiper = new Swiper('.swiper-container', {
+  slidesPerView: 4,
+  direction: getDirection(),
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+  },
+  on: {
+    resize: function () {
+      swiper.changeDirection(getDirection());
+    }
+  }
+});
+
+function getDirection() {
+  var windowWidth = window.innerWidth;
+  var direction = window.innerWidth <= 760 ? 'vertical' : 'horizontal';
+
+  return direction;
+}
+
+
+
+
+//AOS
